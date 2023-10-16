b40848e3f2fb67ca3e60c45f161f885569a319d7
Ahmed Shalash <ah.shalash@gmail.com>
2023-10-16 16:12:32 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(dividend, divisor) {
+    return dividend / divisor;
+  }
+  
+  const square = (num) => num * num;
+  
+  const add = (a, b) => a + b;
+  
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
+  
